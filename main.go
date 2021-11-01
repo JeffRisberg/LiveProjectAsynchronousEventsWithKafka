944d58bde33158fd8d2e9ebc54e5c42809df4b9b
JeffRisberg <j.s.risberg@gmail.com>
2021-10-31 20:53:44 -0700

    wrote up notes on milestone, after researching Go Web and Kafka.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"io"
+)
+
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (n int, err error) {
+	fmt.Println(bs)
+	fmt.Println("just wrote this many bytes:", len(bs))
+	return len(bs), nil
+}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	/*
+	bs := make([]byte, 99999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+	*/
+
+	/*
+	io.Copy(os.Stdout, resp.Body)
+	 */
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
