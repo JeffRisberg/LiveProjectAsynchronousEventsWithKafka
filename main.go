3a0f37b767201e024aecd8ee492489c18bceea3a
JeffRisberg <j.s.risberg@gmail.com>
2021-11-07 23:50:43 -0800

    added example of go server

--- main.go
+++ main.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
-	"os"
-	"io"
 )
 
-type logWriter struct{}
+func charities(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Here are your charities"))
+}
 
-func (logWriter) Write(bs []byte) (n int, err error) {
-	fmt.Println(bs)
-	fmt.Println("just wrote this many bytes:", len(bs))
-	return len(bs), nil
+func donors(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Here are your donors"))
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+	t.Execute(w, daysOfWeek)
+}
+
+type Data struct {
+	Title   string
+	Results []string
+	Other   []int
+}
+
+func process2(w http.ResponseWriter, r *http.Request) {
+	data := &Data{"Lord of the Rings", []string{"a", "b", "c"}, []int{1, 2, 3}}
+	t, _ := template.ParseFiles("tmpl2.html")
+	t.Execute(w, data)
 }
 
 func main() {
-	resp, err := http.Get("http://google.com")
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
+	http.HandleFunc("/charities", charities)
+	http.HandleFunc("/donors", donors)
+	http.HandleFunc("/process", process)
+	http.HandleFunc("/process2", process2)
+
+	http.Handle("/", http.FileServer(http.Dir("./src")))
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
 	}
 
-	/*
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
-	*/
-
-	/*
-	io.Copy(os.Stdout, resp.Body)
-	 */
-
-	lw := logWriter{}
-
-	io.Copy(lw, resp.Body)
 }
