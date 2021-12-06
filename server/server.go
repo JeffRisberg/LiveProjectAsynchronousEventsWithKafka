9e7f77f8fa9c8a7d9e8b1c1c982d17d94650b01f
JeffRisberg <j.s.risberg@gmail.com>
2021-12-05 20:10:53 -0800

    add code to convert from received JSON to Order object (in Go), then validate, then create event.  Finally, publish the event.

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/JeffRisberg/LiveProjectAsynchronousEventsWithKafka/config"
 	"github.com/JeffRisberg/LiveProjectAsynchronousEventsWithKafka/events"
@@ -20,28 +21,102 @@ type Server struct {
 	Port int
 }
 
-func health(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("The order service is running"))
+// Root returns a HTTP 200 status code
+func Root(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
 
-func orders(w http.ResponseWriter, r *http.Request) {
-	topic := config.OrderReceivedTopicName
+// Health returns a HTTP 200 status code indicating the service is alive
+func Health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
 
-	var order = models.Order{
-		ID: uuid.New(),
+// ReceiveOrder handler will accept an order, validate the payload and publish an OrderReceived event to Kafka.
+// returns a HTTP 201 status code indicating an order was created
+func ReceiveOrder(w http.ResponseWriter, r *http.Request) {
+	var o models.Order
+	// Create a new ID for the order
+	o.ID = uuid.New()
+
+	var err error
+
+	if err = json.NewDecoder(r.Body).Decode(&o); err != nil {
+		log.Error(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
 	}
 
+	log.WithField("order", o).Info("received new order")
+
+	if err = validate(o); err != nil {
+		log.WithField("orderID", o.ID).Error(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
+	e := translateOrderToEvent(o)
+
+	log.WithField("event", e).Info("transformed order to event")
+
+	if err = publisher.PublishEvent(e, config.OrderReceivedTopicName); err != nil {
+		log.WithField("orderID", o.ID).Error(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
+
+	log.WithField("event", e).Info("published event")
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+// Validates the order payload has the necessary information and returns an error if it is invalid
+func validate(o models.Order) error {
+	if len(o.Products) == 0 {
+		return fmt.Errorf("there are no products in the order")
+	}
+
+	for i, p := range o.Products {
+		if len(p.ProductCode) == 0 {
+			return fmt.Errorf("product code is required for product [%d]", i)
+		}
+
+		if p.Quantity <= 0 {
+			return fmt.Errorf("quantity should be greater than zero for product [%s]", p.ProductCode)
+		}
+	}
+
+	if len(o.Customer.EmailAddress) == 0 {
+		return fmt.Errorf("email address is required")
+	}
+
+	if len(o.Customer.ShippingAddress.Line1) == 0 {
+		return fmt.Errorf("shipping address line 1 is required")
+	}
+
+	if len(o.Customer.ShippingAddress.City) == 0 {
+		return fmt.Errorf("shipping address city is required")
+	}
+
+	if len(o.Customer.ShippingAddress.PostalCode) == 0 {
+		return fmt.Errorf("shipping address postal code is required")
+	}
+
+	return nil
+}
+
+func translateOrderToEvent(o models.Order) events.Event {
 	var event = events.OrderReceived{
 		EventBase: events.BaseEvent{
 			EventID:        uuid.New(),
 			EventTimestamp: time.Now(),
 		},
-		EventBody: order,
+		EventBody: o,
 	}
 
-	publisher.PublishEvent(event, topic)
-
-	w.Write([]byte("Message sent to topic"))
+	return event
 }
 
 // ListenAndServe will start the web server and listen for requests
@@ -57,8 +132,9 @@ func (s *Server) ListenAndServe() error {
 	r.Use(middleware.Recoverer) // handles any unhandles errors and returns a 500
 
 	// setup supported routes
-	r.Get("/", orders)
-	r.Get("/health", health)
+	r.Get("/", Root)
+	r.Get("/health", Health)
+	r.Post("/orders", ReceiveOrder)
 
 	address := fmt.Sprintf(":%d", s.Port)
 	log.WithField("address", address).Info("server starting")
