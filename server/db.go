f4d9be8e022c743b4acb491e5cecad7d1a73ffcf
JeffRisberg <j.s.risberg@gmail.com>
2021-12-25 17:46:58 -0800

    in progress on transition to MariaDB

--- server/db.go
+++ server/db.go
@@ -1,15 +1,12 @@
 package server
 
 import (
-	"context"
-	"errors"
-	"fmt"
+	"database/sql"
 	"time"
 
 	"github.com/JeffRisberg/LiveProjectAsynchronousEventsWithKafka/config"
 	"github.com/JeffRisberg/LiveProjectAsynchronousEventsWithKafka/events"
-	"github.com/gopsql/mysql"
-	"github.com/gopsql/psql"
+	_ "github.com/go-sql-driver/mysql"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -32,50 +29,38 @@ func NewDB() DB {
 }
 
 // EventExists will check to see if an event has already been processed
-func (db DB) EventExists(event events.Event, tx pgx.Tx) (bool, error) {
-	var id string
-	var err error
+func EventExists(event events.Event) (bool, error) {
 
-	if err = tx.QueryRow(context.Background(), "select id from events.processed_events where id=$1 and event_name=$2", event.ID(), event.Name()).Scan(&id); err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			log.WithField("code", pgErr.Code).
-				WithField("message", pgErr.Message).
-				Error("encountered an issue querying for the event")
-			return false, err
-		}
-		return false, nil
+	db, err := sql.Open("mysql", "developer:123456@/dbname")
+	if err != nil {
+		panic(err)
 	}
+	defer db.Close()
 
-	return len(id) > 0, nil
+	rows, err := db.Query("select id from events.processed_events where id=$1 and event_name=$2", event.ID(), event.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	return false, nil
 }
 
 // InsertEvent will insert a row into the processed_events table to indicate an event was processed.
-func (db DB) InsertEvent(event events.Event, tx pgx.Tx) error {
-	var err error
+func InsertEvent(event events.Event) error {
+	db, err := sql.Open("mysql", "developer:123456@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	if _, err = tx.Exec(context.Background(), "insert into events.processed_events (id, event_name, processed_timestamp) values ($1, $2, $3)", event.ID(), event.Name(), time.Now()); err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			log.WithField("code", pgErr.Code).
-				WithField("message", pgErr.Message).
-				Error("encountered an issue inserting the event into the DB")
-		}
-
-		return err
+	stmt, err := db.Prepare("insert into events.processed_events (id, event_name, processed_timestamp) values ($1, $2, $3)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = stmt.Exec(event.ID(), event.Name(), time.Now())
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return nil
-}
-
-// Connect creates a new connection to the database
-func (db DB) Connect() (*pgx.Conn, error) {
-	url := fmt.Sprintf("mysql://%s:%s@%s/%s", db.Username, db.Password, db.Address, db.Database)
-	log.WithField("url", url).Info("attempting to connect to DB")
-
-	conn := mysql.MustOpen(url)
-
-	log.Info("successfully connected to DB")
-
-	return conn, nil
+	return err
 }
